89a9a92da9aaba479ef41bd650d242c3121ecc8f
dantezhu <dantezhu@qq.com>
2016-02-29 18:00:19 +0800

    logger

--- zmq_server/share/log.py
+++ zmq_server/share/log.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import logging
-
 import constants
 
+
+LOG_FORMAT = '\n'.join((
+    '/' + '-' * 80,
+    '[%(levelname)s][%(asctime)s][%(process)d:%(thread)d][%(filename)s:%(lineno)d %(funcName)s]:',
+    '%(message)s',
+    '-' * 80 + '/',
+))
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(LOG_FORMAT))
 logger = logging.getLogger(constants.NAME)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
