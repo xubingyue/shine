b3de49d3ccf031558a2392c1cd57474c79c23c51
dantezhu <dantezhu@qq.com>
2016-03-01 17:01:27 +0800

    login

--- examples/test_run/run_worker.py
+++ examples/test_run/run_worker.py
@@ -11,6 +11,15 @@ import constants
 
 worker = Worker(Box)
 
+
+@worker.route(2)
+def login(request):
+    request.write_to_client(dict(
+        ret=100,
+        body='ok'
+    ))
+
+
 if __name__ == '__main__':
     worker.run(
         constants.GATEWAY_WORKER_ADDRESS_LIST,
