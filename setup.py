9b07648af8e824155b58ef98471e8a6d7f0856d7
dantezhu <dantezhu@qq.com>
2016-03-04 18:27:15 +0800

    版本号

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shine",
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
