a4209454fdffd93800e7a4d4f24301fd1bf6747e
dantezhu <dantezhu@qq.com>
2016-03-04 18:45:10 +0800

    支持打印有颜色的log

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    install_requires=[],
+    install_requires=['colorlog'],
     url="https://github.com/dantezhu/shine",
     license="MIT",
     author="dantezhu",
--- shine/share/log.py
+++ shine/share/log.py
@@ -2,6 +2,7 @@
 
 import logging
 import constants
+import colorlog
 
 
 LOG_FORMAT = '\n'.join((
@@ -11,8 +12,16 @@ LOG_FORMAT = '\n'.join((
     '-' * 80 + '/',
 ))
 
+COLOR_LOG_FORMAT = '%(log_color)s' + LOG_FORMAT
+
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(LOG_FORMAT))
+handler.setFormatter(colorlog.ColoredFormatter(COLOR_LOG_FORMAT, log_colors={
+    'DEBUG':    'cyan',
+    'INFO':     'green',
+    'WARNING':  'yellow',
+    'ERROR':    'red',
+    'CRITICAL': 'red',
+}))
 logger = logging.getLogger(constants.NAME)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
