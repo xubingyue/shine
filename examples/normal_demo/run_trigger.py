2d9c2df7844e9de13a16db938c9c189f0dc379c8
dantezhu <dantezhu@qq.com>
2016-03-04 19:07:59 +0800

    我了个擦，没支持全量用户。。

---
+++ examples/normal_demo/run_trigger.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.insert(0, '../../')
+from netkit.box import Box
+import time
+from shine import Trigger
+
+import constants
+
+
+def handle():
+    trigger = Trigger(Box, constants.FORWARDER_INPUT_ADDRESS_LIST)
+
+    box = Box()
+    box.cmd = 3
+
+    trigger.write_to_users([
+        [(-1,), box]
+    ])
+
+
+def main():
+    handle()
+
+if __name__ == '__main__':
+    main()
--- shine/__init__.py
+++ shine/__init__.py
@@ -5,4 +5,5 @@ __version__ = '0.1.2'
 from gateway.gateway import Gateway
 from forwarder.forwarder import Forwarder
 from worker.worker import Worker
+from trigger.trigger import Trigger
 from share.log import logger
