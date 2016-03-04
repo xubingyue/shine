7f40690446b80f5fdb73eb284896cc089905d1e6
dantezhu <dantezhu@qq.com>
2016-03-04 17:31:52 +0800

    shine，名字终于定了

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="shine",
+    version='0.1.1',
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=[],
+    url="https://github.com/dantezhu/shine",
+    license="MIT",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="shine",
+)
