034c3486feeee0527e5843d03c1440ee5483250b
dantezhu <dantezhu@qq.com>
2016-03-04 20:42:28 +0800

    放的位置

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    scripts=['shine/bin/shinectl.py'],
     install_requires=['colorlog'],
     url="https://github.com/dantezhu/shine",
     license="MIT",
