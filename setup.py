50bd1175d8606905955920955726d823adc0569c
dantezhu <dantezhu@qq.com>
2016-03-05 16:29:06 +0800

    应该是pyzmq

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     scripts=['shine/bin/shine'],
-    install_requires=['events', 'zmq', 'netkit', 'click', 'setproctitle'],
+    install_requires=['events', 'pyzmq', 'netkit', 'click', 'setproctitle'],
     url="https://github.com/dantezhu/shine",
     license="MIT",
     author="dantezhu",
