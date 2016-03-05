2e2e74c45d43888167936aaf1196687a31cad5ce
dantezhu <dantezhu@qq.com>
2016-03-05 16:44:46 +0800

    blueprint忘记导入了

--- shine/__init__.py
+++ shine/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 from gateway.gateway import Gateway
 from forwarder.forwarder import Forwarder
 from worker.worker import Worker
+from worker.blueprint import Blueprint
 from trigger.trigger import Trigger
 from share.log import logger
