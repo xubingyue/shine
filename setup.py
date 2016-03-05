1aeaceedbd66ad26a52afff6e69c41bdd72f80ad
dantezhu <dantezhu@qq.com>
2016-03-05 15:44:23 +0800

    不需要py后缀了

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="shine",
-    version='0.1.7',
+    version='0.1.8',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    scripts=['shine/bin/shined.py'],
+    scripts=['shine/bin/shine'],
     install_requires=['events', 'zmq', 'netkit', 'click', 'setproctitle'],
     url="https://github.com/dantezhu/shine",
     license="MIT",
