d3e188e7171db1ad4a4ee8fcceca1b2a36a03b48
dantezhu <dantezhu@qq.com>
2016-03-07 19:28:50 +0800

    版本升级

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shine",
-    version='0.1.19',
+    version='0.1.20',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
