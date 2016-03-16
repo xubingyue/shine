f43ba9b66c33f2095e3923806f571a4e2aeaa73e
dantezhu <dantezhu@qq.com>
2016-03-16 18:58:55 +0800

    redis的依赖

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     scripts=['shine/bin/shine'],
-    install_requires=['events', 'pyzmq', 'netkit', 'click', 'setproctitle'],
+    install_requires=['events', 'pyzmq', 'netkit', 'click', 'setproctitle', 'redis'],
     url="https://github.com/dantezhu/shine",
     license="MIT",
     author="dantezhu",
